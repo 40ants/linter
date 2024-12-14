2c1f0752a47f693e897eee301a18097234aa41d7
Alexander Artemenko <svetlyak.40wt@gmail.com>
2024-12-14 08:23:40 +0000

    Fixed doc issue.

--- docs/changelog.lisp
+++ docs/changelog.lisp
@@ -12,7 +12,7 @@
                               "DYNAMIC_SPACE_SIZE"
                               "HTTP"))
   (0.4.0 2024-12-14
-         "* Now DYNAMIC_SPACE_SIZE env variable can be used to control amount of memory available to SBCL, by default it is 1Gb.")
+         "* Now `DYNAMIC_SPACE_SIZE` env variable can be used to control amount of memory available to SBCL, by default it is 1Gb.")
   (0.3.0 2023-12-01
          "* Fixed issue when package was mentioned as unused whereas it was used inside a backquoted part of the macro.")
   (0.2.0 2023-11-19
