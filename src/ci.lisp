8ff15c08b01406bfe4a0ec46c0c57f9ed1223c1c
Alexander Artemenko <svetlyak.40wt@gmail.com>
2025-12-24 12:40:18 +0300

    Updated CI workflows.

--- src/ci.lisp
+++ src/ci.lisp
@@ -16,6 +16,10 @@
   :on-pull-request t
   :cache t
   :jobs ((40ants-ci/jobs/linter:linter
+          ;; Until the issue with
+          ;; Bug in readtable iterators or concurrent access?
+          ;; will be resolved in the named-readtables.
+          :lisp "sbcl-bin/2.5.10"
           :asdf-systems ("40ants-linter"
                          "40ants-linter-docs"
                          "40ants-linter-tests"))))
@@ -25,7 +29,12 @@
   :by-cron "0 10 * * 1"
   :on-pull-request t
   :cache t
-  :jobs ((build-docs :asdf-system "40ants-linter-docs")))
+  :jobs ((build-docs
+          ;; Until the issue with
+          ;; Bug in readtable iterators or concurrent access?
+          ;; will be resolved in the named-readtables.
+          :lisp "sbcl-bin/2.5.10"
+          :asdf-system "40ants-linter-docs")))
 
 
 (defworkflow ci
@@ -34,6 +43,9 @@
   :on-pull-request t
   :cache t
   :jobs ((run-tests
-          :lisp ("sbcl-bin"
+          ;; Until the issue with
+          ;; Bug in readtable iterators or concurrent access?
+          ;; will be resolved in the named-readtables.
+          :lisp ("sbcl-bin/2.5.10"
                  "ccl-bin")
           :coverage t)))
